776e1e8777d80aca4e59d8a91f43866d64491f48
KID <50666207+KID421@users.noreply.github.com>
2022-07-27 16:29:52 +0800

    Update 完成音效控制與發佈

---
+++ VuforiaKnight_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void KID.AR.Vuforia.CardManager::Awake()
+extern void CardManager_Awake_m409B64827DC449AAE76B1FC70805EA2413087689 (void);
+// 0x00000002 System.Void KID.AR.Vuforia.CardManager::OnJumpPressed(Vuforia.VirtualButtonBehaviour)
+extern void CardManager_OnJumpPressed_m2C3C1F60B3B4D896435C7CB668236DD7CA0610B2 (void);
+// 0x00000003 System.Void KID.AR.Vuforia.CardManager::CardFound()
+extern void CardManager_CardFound_mAC24D55C22BBCAB1E1F71E2BF0FAE53939447031 (void);
+// 0x00000004 System.Void KID.AR.Vuforia.CardManager::CardLost()
+extern void CardManager_CardLost_mC610EF3FC5C432DA08E170AB7D624512790A3243 (void);
+// 0x00000005 System.Void KID.AR.Vuforia.CardManager::Attack()
+extern void CardManager_Attack_m15FDB6DB32394CDFAE32232F1D4EF4D1553CFC46 (void);
+// 0x00000006 System.Void KID.AR.Vuforia.CardManager::.ctor()
+extern void CardManager__ctor_mE19AC721FB44F9E554BF9D0B6E9E5CC9978F9C2E (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	CardManager_Awake_m409B64827DC449AAE76B1FC70805EA2413087689,
+	CardManager_OnJumpPressed_m2C3C1F60B3B4D896435C7CB668236DD7CA0610B2,
+	CardManager_CardFound_mAC24D55C22BBCAB1E1F71E2BF0FAE53939447031,
+	CardManager_CardLost_mC610EF3FC5C432DA08E170AB7D624512790A3243,
+	CardManager_Attack_m15FDB6DB32394CDFAE32232F1D4EF4D1553CFC46,
+	CardManager__ctor_mE19AC721FB44F9E554BF9D0B6E9E5CC9978F9C2E,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	5045,
+	4003,
+	5045,
+	5045,
+	5045,
+	5045,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
